233cc7ffd7b54657bfecdcb16d594c013f3bda5f
Gavin Klibowitz <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2023-05-16 17:33:18 -0500

    Update custom.sh

--- custom.sh
+++ custom.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # This script combines many many different linux security tools into one script that runs on system startup similar to OOBE of macOS but on GNU/Linux
 sudo freshclam
-sudo clamscan -r /**
+sudo clamscan -r /** | grep FOUND >> ./file.txt
